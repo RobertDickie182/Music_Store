56e54d08239629e67027f6195fd03e7cf52e394e
RobertDickie182 <robertdanieldickie@gmail.com>
2023-10-03 14:34:05 +0100

    interfaces added

--- src/main/java/MusicShop/Guitar.java
+++ src/main/java/MusicShop/Guitar.java
@@ -5,8 +5,36 @@ import Behaviours.ISell;
 
 public class Guitar extends Instrument implements IPlay, ISell {
     private String model;
-
     private int numberOfStrings;
     private int costPrice;
+    private int retailPrice;
 
+    public Guitar(String colour, String type, String model, int numberOfStrings, int costPrice, int retailPrice) {
+        super(colour, "String");
+        this.model = model;
+        this.numberOfStrings = numberOfStrings;
+        this.costPrice = costPrice;
+        this.retailPrice = retailPrice;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getNumberOfStrings() {
+        return numberOfStrings;
+    }
+
+    public int getCostPrice() {
+        return costPrice;
+    }
+
+    public int getRetailPrice() {
+        return retailPrice;
+    }
+
+    @Override
+    public String play() {
+        return "Kerrang!!!";
+    }
 }
