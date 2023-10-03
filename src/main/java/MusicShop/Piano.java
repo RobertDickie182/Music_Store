cfd5e01f838c53bf93ffa25295c36eac78402b1b
RobertDickie182 <robertdanieldickie@gmail.com>
2023-10-03 14:43:23 +0100

    instruments added

--- src/main/java/MusicShop/Piano.java
+++ src/main/java/MusicShop/Piano.java
@@ -1,7 +1,35 @@
 package MusicShop;
 
-public class Piano extends Instrument {
-    public Piano(String colour, String type) {
-        super(colour, type);
+import Behaviours.IPlay;
+import Behaviours.ISell;
+
+public class Piano extends Instrument implements ISell, IPlay {
+
+    private String manufacturer;
+    private int buyPrice;
+    private int sellPrice;
+
+    public Piano(String colour, String type, String manufacturer, int buyPrice, int sellPrice) {
+        super(colour, "Keyboard");
+        this.manufacturer = manufacturer;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public int getBuyPrice() {
+        return buyPrice;
+    }
+
+    public int getSellPrice() {
+        return sellPrice;
+    }
+
+    @Override
+    public String play() {
+        return "Plink Plonk";
     }
 }
